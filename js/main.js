715e7f89ea2e34685e1f95d0c76837f49d170dc9
Artur Kushner <kushnerartur@gmail.com>
2024-05-29 10:27:30 +0100

    feat: add dark mode my project

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
   const btnDarkMode = document.querySelector(".dark-mode-btn");
+  const bodyElement = document.body;
   btnDarkMode.onclick = function () {
     btnDarkMode.classList.toggle("dark-mode-btn--active");
+    bodyElement.classList.toggle("dark");
   };
 });
